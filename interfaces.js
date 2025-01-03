bd60b998e57fe87089df2fe2a134f8d17907b0c6
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-03 00:43:51 -0300

    creo interfaces

---
+++ interfaces.js
@@ -0,0 +1,15 @@
+"use strict";
+let dev = {
+    nombre: 'Pedro',
+    tecnologias: ['React', 'Angular', 'Svelte'],
+    tomaCafe: true
+};
+let dev2 = {
+    nombre: "Federico",
+    tecnologias: ['HTML', 'Cobol'],
+    tomaCafe: null
+};
+function enviarCv(programador) {
+    console.log(`Este curriculum es de ${programador.nombre}`);
+}
+enviarCv(dev);
